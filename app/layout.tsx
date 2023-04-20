1e9a2fe0ca427428cebd4b7532081f85325ca8a6
GoodnessObi <goodnessobi@gmail.com>
2023-04-20 12:14:46 +0100

    chore: install and setup tailwind

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import './globals.css';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import { Alata } from 'next/font/google';
-import StyledComponentsRegistry from '@/lib/registry';
 
 const alata = Alata({ weight: '400', preload: false });
 
@@ -21,13 +20,11 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang='en'>
-			<StyledComponentsRegistry>
-				<body className={alata.className}>
-					<Header />
-					<main>{children}</main>
-					<Footer />
-				</body>
-			</StyledComponentsRegistry>
+			<body className={alata.className}>
+				<Header />
+				<main>{children}</main>
+				<Footer />
+			</body>
 		</html>
 	);
 }
