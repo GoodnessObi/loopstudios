9feede673a9c2c0d126d067dd446515d38c4e4c4
GoodnessObi <goodnessobi@gmail.com>
2023-04-21 12:45:31 +0100

    chore: fix typing for deployment

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
 import About from '@/components/About';
-import Footer from '@/components/Footer';
 import Hero from '@/components/Hero';
 import Products from '@/components/Products';
-import { Josefin_Sans } from 'next/font/google';
-const josefin = Josefin_Sans({ weight: '300', preload: false });
+// import { Josefin_Sans } from 'next/font/google';
+// const josefin = Josefin_Sans({ weight: '300', preload: false });
 
-export default function Home({ ...props }) {
+export default function Home() {
 	return (
 		<>
 			<Hero />
